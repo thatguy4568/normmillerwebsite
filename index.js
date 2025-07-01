e69cd420b4252fac57a9958d2f5b935c3ee5e271
wenxiong342@gmail.com <wenxiong342@gmail.com>
2025-07-01 10:21:37 -0400

    Update Format

--- index.js
+++ index.js
@@ -2,22 +2,25 @@ import express from "express";
 import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+import path from "path";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
 
+//app.use('/static', express.static(path.join(__dirname, 'public')));
+
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
-  });
-  
-  app.post("/submit", function(req, res){
-    //Do something with the data
-    console.log(req.body);
-    res.sendStatus(201);
-  });
-  
-  app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
-  });
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/submit", function (req, res) {
+  //Do something with the data
+  console.log(req.body);
+  res.sendStatus(201);
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
