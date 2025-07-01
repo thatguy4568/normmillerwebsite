36f2fca9650efbec1a793d9f05560035e4e5ebf0
wenxiong342@gmail.com <wenxiong342@gmail.com>
2025-07-01 12:12:53 -0400

    Pull from Chris

--- index.js
+++ index.js
@@ -2,25 +2,38 @@ import express from "express";
 import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import path from "path";
-
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+let notes = []; 
 
-//app.use('/static', express.static(path.join(__dirname, 'public')));
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", (req, res) =>{
+    res.render("index", { notes });
 });
+ app.post("/new", (req, res) =>{
+    const {title, content} = req.body;
+    const newNote = {
+        id: Date.now(),
+        title, content,
+    };
+    newNote.title = newNote.title.slice(0,10) + "...";
+    notes.push(newNote);
+    res.redirect("/")
+})
 
-app.post("/submit", function (req, res) {
-  //Do something with the data
-  console.log(req.body);
-  res.sendStatus(201);
-});
+app.post("/delete/:id", (req, res) => {
+    const idToDelete = parseInt(req.params.id);
+    notes = notes.filter(note => note.id !== idToDelete);
+    res.redirect("/");
+  });
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+app.listen(port, (req, res)=>{
+    console.log(`running on port ${port}`);
 });
